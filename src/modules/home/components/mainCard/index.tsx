07a044a4442cc14aa70653de352a3a7433a29bfa
Lúcio Henrique <lucions96@gmail.com>
2021-05-13 17:32:25 -0300

    Adicionando textos no card

--- src/modules/home/components/mainCard/index.tsx
+++ src/modules/home/components/mainCard/index.tsx
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
 import styled from "styled-components/native";
 import { Sizes } from "../../../../commons";
-import { SYTextInput } from "../../../../components";
+import { SYText, SYTextInput } from "../../../../components";
 import { CircularProgress } from "../circularProgres";
 
 export const MainCard = ({
@@ -16,6 +16,26 @@ export const MainCard = ({
       <ChartContainer>
         <CircularProgress value={cardValue} maxValue={maxCardValue} />
       </ChartContainer>
+      <TextContainer>
+        <SYText
+          text="Resumo da discplina"
+          size={Sizes.fontScale(16)}
+          fontWeight="bold"
+        />
+        <SYText text="Eng. de Software" size={Sizes.fontScale(12)} secondary />
+        <SYText
+          text="próxima entrega:"
+          size={Sizes.fontScale(12)}
+          marginTop={Sizes.horizontalScale(12)}
+          secondary
+        />
+        <SYText
+          text="10/04/1996"
+          size={Sizes.fontScale(12)}
+          secondary
+          fontWeight="bold"
+        />
+      </TextContainer>
     </Container>
   );
 };
@@ -28,6 +48,7 @@ const Container = styled.View`
   border-color: ${({ theme }) => `${theme.secondary_text}65`};
   border-radius: ${Sizes.fontScale(10)}px;
   overflow: hidden;
+  flex-direction: row;
 `;
 
 const ChartContainer = styled.View`
@@ -35,4 +56,10 @@ const ChartContainer = styled.View`
   height: ${Sizes.horizontalScale(110)}px;
   align-items: center;
   justify-content: center;
+`;
+
+const TextContainer = styled.View`
+  width: ${Sizes.horizontalScale(317) * 0.65}px;
+  height: ${Sizes.horizontalScale(110)}px;
+  padding: ${Sizes.fontScale(18)}px;
 `;
